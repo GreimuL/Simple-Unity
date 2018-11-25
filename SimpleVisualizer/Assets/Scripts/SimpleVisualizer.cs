5e8fcb55bc0a7e8c11cd28f60d4fdf10eda1148e
GreimuL <yoseib1998@naver.com>
2018-11-25 21:55:43 +0900

    Fix Visualizer angle

--- SimpleVisualizer/Assets/Scripts/SimpleVisualizer.cs
+++ SimpleVisualizer/Assets/Scripts/SimpleVisualizer.cs
@@ -3,29 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleVisualizer : MonoBehaviour {
+    public const int BarCntSize = 64;
+    public const int SpectrumSize = 128;
     public AudioSource music;
     public GameObject visual;
     public Transform visP;
-    public RectTransform[] visuals = new RectTransform[64];
-    public float[] spectrum = new float[64];
+    public RectTransform[] visuals = new RectTransform[BarCntSize];
+    public float[] spectrum = new float[SpectrumSize];
     // Use this for initialization
     void Start () {
-        float angle = 0;
-        for(int i = 0; i < 64; i++){
+        float angle = 0.0f;
+        for(int i = 0; i < BarCntSize; i++){
             visuals[i] = Instantiate(visual).GetComponent<RectTransform>();
-            visuals[i].localPosition = new Vector2(3*Mathf.Cos(angle), 3*Mathf.Sin(angle));
-            visuals[i].localRotation = Quaternion.Euler(0,0,angle);
+            visuals[i].localPosition = new Vector2(3*Mathf.Cos(Mathf.Deg2Rad*angle), 3*Mathf.Sin(Mathf.Deg2Rad * angle));
+            visuals[i].localRotation = Quaternion.Euler(0,0,angle+90);
             visuals[i].parent = visP;
             visuals[i].sizeDelta = new Vector2(1, 1);
-            angle += 360 / 64;
+            angle += 360.0f / (float)BarCntSize;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        music.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        music.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
        
-        for(int i = 0; i < 64; i++){
+        for(int i = 0; i < BarCntSize; i++){
             if (spectrum[i] * 5 > 0.5f){
                 visuals[i].sizeDelta = new Vector2(0.2f, 0.5f);
             }
